5fcb1bb271a7c306da0c389bdcd1280ff0f3715f
nickgiant <nickgiant@yahoo.com>
2019-10-19 16:10:40 +0300

    mass copy from other company corrections

--- src/com/tool/utils/VariablesGlobal.java
+++ src/com/tool/utils/VariablesGlobal.java
@@ -53,7 +53,7 @@ public class VariablesGlobal
 
     public static  String appName = "BusinessEye";
     public static  String appLeadVersion = "1";
-    public static  String appSubVersion = "0.2571";
+    public static  String appSubVersion = "0.2573";
     public static  String appVersionYear = "2019/10";
     public static  String appProduct = "0"; // 0 all
     public static  String appProductCaption = "";
